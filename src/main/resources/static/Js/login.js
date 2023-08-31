e0a99369df3a9a80274cb7ae88290a822f3ecd7c
Arthur <arthurpadlle@hotmail.fr>
2023-08-31 14:41:15 +0200

    Page login

--- src/main/resources/static/Js/login.js
+++ src/main/resources/static/Js/login.js
@@ -1,5 +1,52 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+onload = ()=>{
+    const registerButton = document.getElementById('register')
+    const loginButton = document.getElementById('login')
+    const container = document.getElementById('containerLogin')
+    inputs = document.getElementsByTagName("input");
+    inputs.user.addEventListener("input", verifierUtlisateur);
+    inputs.mail.addEventListener("input", verifierMail);
+    inputs.mdpInfo.addEventListener("input", verifierMdp);
+
+    loginForm = document.getElementById("subForm");
+
+
+    registerButton.onclick = function(){
+        container.className = 'active'
+    }
+    loginButton.onclick = function(){
+        container.className = 'close'
+    }
+
+
+
+}
+
+function verifierUtlisateur() {
+    //dans l'input "user", j'accede a ce qui a été tapé dedans
+    if (inputs.user.value.length < 3) {
+        inputs.user.setCustomValidity("C'est trop court");
+    }else{
+        inputs.user.setCustomValidity("");
+    }
+}
+
+function verifierMail() {
+
+    if (!(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(inputs.mail.value))) {
+        inputs.mail.setCustomValidity("Toujours pas ça Jean-Paul!");
+    } else {
+        inputs.mail.setCustomValidity("");
+    }
+}
+function verifierMdp() {
+
+    if (!(/^((?=.*?[a-zA-Z])(?=.*?[0-9])(?=.*[#?!@$%^&*-])).{6,}$/.test(inputs.mdpInfo.value)) ){
+        inputs.mdpInfo.setCustomValidity("T'es mauvais Bernard!");
+    }else{
+        inputs.mdpInfo.setCustomValidity("");
+    }
+}
+
 // function verifierUtlisateur() {
 //     //dans l'input "user", j'accede a ce qui a été tapé dedans
 //     if (inputs.user.value.length < 3) {
@@ -18,6 +65,5 @@
 //     }
 //
 // }
-=======
->>>>>>> 5d2148ed383f4fc801ed72da5ac2538291e7fb5c
+
 
